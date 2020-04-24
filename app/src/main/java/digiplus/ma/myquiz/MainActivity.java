5cbcba43c8786c8f6c75dcc94e740eb9a3820641
zelbatmi <zakari_a@icloud.com>
2020-04-24 16:32:32 +0000

    V1.0

--- app/src/main/java/digiplus/ma/myquiz/MainActivity.java
+++ app/src/main/java/digiplus/ma/myquiz/MainActivity.java
@@ -3,6 +3,11 @@ package digiplus.ma.myquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Submit(View view) {
+        // Get use name
+        EditText nameField = (EditText) findViewById(R.id.Name);
+        Editable nameEditable = nameField.getText();
+        String name = nameEditable.toString();
+        // Log.v("MainActivity","User Name : ", + name);
+        DisplayAreaONE(name);
+    }
+
+    private void DisplayAreaONE(String Name_) {
+        TextView AreaONE = (TextView) findViewById(R.id.TextArea1);
+        AreaONE.setText(Name_);
     }
 }
