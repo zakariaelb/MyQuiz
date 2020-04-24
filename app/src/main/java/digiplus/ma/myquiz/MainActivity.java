eec28f2d8467dbf421fbddde4d51afd2fea62274
zelbatmi <zakari_a@icloud.com>
2020-04-24 19:07:28 +0000

    V1.0

--- app/src/main/java/digiplus/ma/myquiz/MainActivity.java
+++ app/src/main/java/digiplus/ma/myquiz/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +25,30 @@ public class MainActivity extends AppCompatActivity {
         EditText nameField = (EditText) findViewById(R.id.Name);
         Editable nameEditable = nameField.getText();
         String name = nameEditable.toString();
-        //CheckBox one
+
+        //CheckBox One
         CheckBox AnswerONE = (CheckBox) findViewById(R.id.CheckOne);
-        boolean Checked = AnswerONE.isChecked();
-        String AreaONEMsg = DisplayAreaONE(name, Checked);
+        boolean OneIsChecked = AnswerONE.isChecked();
+
+        //CheckBox Tow
+        CheckBox AnswerTOW = (CheckBox) findViewById(R.id.CheckTow);
+        boolean TowIsChecked = AnswerTOW.isChecked();
+
+        //*************
+        String AreaONEMsg = DisplayAreaONE(name, OneIsChecked, TowIsChecked);
         DisplayAreaONE(AreaONEMsg);
+        button = findViewById(R.id.Submit);
+                button.setVisibility(View.GONE);
     }
 
     private void DisplayAreaONE(String AreaONEMsg) {
         TextView AreaONE = (TextView) findViewById(R.id.TextArea1);
         AreaONE.setText(AreaONEMsg);
     }
-    private String DisplayAreaONE(String name, boolean Checked) {
+    private String DisplayAreaONE(String name, boolean OneIsChecked, Boolean TowIsChecked) {
         String AreaMessage = (name);
-        AreaMessage += "\n" + (Checked);
+        //AreaMessage += "\n 1" + (OneIsChecked);
+        //AreaMessage += "\n 2" + (TowIsChecked);
         return AreaMessage;
     }
 }
