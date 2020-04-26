65fcddc73bf1ed0fb4a427ef491e758196a2bc10
zelbatmi <zakari_a@icloud.com>
2020-04-26 14:23:57 +0000

    V1.0

--- app/src/main/java/digiplus/ma/myquiz/MainActivity.java
+++ app/src/main/java/digiplus/ma/myquiz/MainActivity.java
@@ -2,6 +2,7 @@ package digiplus.ma.myquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.text.Editable;
@@ -14,12 +15,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button, Start, Cancel;
+    EditText EditName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void Submit(View view) {
         // Get use name
         EditText nameField = (EditText) findViewById(R.id.Name);
@@ -38,21 +41,38 @@ public class MainActivity extends AppCompatActivity {
         String AreaONEMsg = DisplayAreaONE(name, OneIsChecked, TowIsChecked);
         DisplayAreaONE(AreaONEMsg);
         button = findViewById(R.id.Submit);
-                button.setVisibility(View.GONE);
+        button.setVisibility(View.GONE);
         Start = findViewById(R.id.Start);
-                Start.setVisibility(View.VISIBLE);
+        Start.setVisibility(View.VISIBLE);
         Cancel = findViewById(R.id.Cancel);
-                Cancel.setVisibility(View.VISIBLE);
+        Cancel.setVisibility(View.VISIBLE);
     }
 
     private void DisplayAreaONE(String AreaONEMsg) {
         TextView AreaONE = (TextView) findViewById(R.id.TextArea1);
         AreaONE.setText(AreaONEMsg);
     }
+
     private String DisplayAreaONE(String name, boolean OneIsChecked, Boolean TowIsChecked) {
-        String AreaMessage = getString(R.string.Welcome ,name);
+        String AreaMessage = getString(R.string.Welcome, name);
         //AreaMessage += "\n 1" + (OneIsChecked);
         //AreaMessage += "\n 2" + (TowIsChecked);
         return AreaMessage;
+    }
+
+    public void Startbtn(View view) {
+        EditName = findViewById(R.id.Name);
+        EditName.setVisibility(View.GONE);
+        String QuestionOne = getString(R.string.QuesionTitle) + "\n" + getString(R.string.QuesionOne);
+        DisplayAreaONE(QuestionOne);
+    }
+
+    public void str(View view) {
+        StartQuiz();
+    }
+
+    private void StartQuiz() {
+        Intent intent = new Intent(this, MainQuiz.class);
+        startActivity(intent);
     }
 }
